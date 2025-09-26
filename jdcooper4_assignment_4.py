8160f4c99f30955f79e7f83ea7e90c5e32404ddd
Jeremiah262007 <jcallday85@gmail.com>
2025-09-26 17:59:41 -0400

    Pass Test Case 1: Initial game setup with required variables

---
+++ jdcooper4_assignment_4.py
@@ -0,0 +1,10 @@
+student_name = "Jeremiah"
+current_gpa = 4.0
+study_hours = 21
+social_points = 50
+stress_level = 67
+
+print("---NCAT: LIFESTYlE---")
+print(f"Welcome {student_name}!")
+print(f"Your current stats are, GPA: {current_gpa}, Study Hours: {study_hours}, Social Points: {social_points}, Stress Level: {stress_level}")
+#
